e3f46afe6844c30f860bad1eba018930c1b9c713
Jake Vivian <jacob@sadsoft.com>
2022-05-26 19:48:33 -0700

    Just keep the syntax highlighting simple

--- app/Nova/Product.php
+++ app/Nova/Product.php
@@ -77,7 +77,7 @@ class Product extends Resource
                 }
 
                 return $content;
-            }),
+            })->language('twig'),
             Text::make('Description')
                 ->hideFromIndex()
                 ->placeholder('Item purchased from Habitat for Humanity')
